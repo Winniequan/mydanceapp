9c1f7bb0323d9448eecb5f39c25673a02bdcee86
Winniequan <quanying26@hotmail.com>
2026-02-13 19:27:37 -0500

    Initial setup

---
+++ src/main/java/com/mydanceapp/mydanceapp/MyDanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.mydanceapp.mydanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDanceAppApplication.class, args);
+	}
+
+}
