f3257979a7c210424684e9e9bdbc218bb90f9d5a
Winniequan <quanying26@hotmail.com>
2026-02-13 19:43:11 -0500

    add UserRepositoty

--- src/main/java/com/mydanceapp/mydanceapp/user/UserRepository.java
+++ src/main/java/com/mydanceapp/mydanceapp/user/UserRepository.java
@@ -1,5 +1,10 @@
 package com.mydanceapp.mydanceapp.user;
 
-public class UserRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
 }
